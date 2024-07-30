4fc7c49d74e38bdaa537d27170d598a3b1d50774
Malik <malikhussein229@gmail.com>
2024-07-30 18:09:04 +0300

    Task#0 Basic list of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+  return [
+    { id: 1, firsName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firsName: 'James', location: 'Columbia' },
+    { id: 5, firsName: 'Serena', location: 'San Francisco' },
+  ];
+}
