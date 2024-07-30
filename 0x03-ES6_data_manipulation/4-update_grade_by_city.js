359e960017546be8984b1bf6542c38ab64d32acb
Malik <malikhussein229@gmail.com>
2024-07-30 20:42:30 +0300

    Task#4 Combine

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+export default function updateStudentGradeByCity(array, city, newGrades) {
+  if (!(array instanceof Array) || (typeof city !== 'string') || !(newGrades instanceof Array)) {
+    return [];
+  }
+
+  return array.filter((student) => student.location === city).map((student) => {
+    const studentGrade = newGrades.find((grade) => grade.studentId === student.id);
+    const grade = studentGrade ? studentGrade.grade : 'N/A';
+    return { ...student, grade };
+  })
+}
