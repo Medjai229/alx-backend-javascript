01708b330e1bbf7766207cf227a7dbfb689a1c9b
Malik <malikhussein229@gmail.com>
2024-07-30 18:36:39 +0300

    Task#2 Filter

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,7 @@
+export default function getStudentsByLocation(array, city) {
+  if (!(array instanceof Array) || (typeof city !== 'string')) {
+    return [];
+  }
+
+  return array.filter((student) => student.location === city);
+}
