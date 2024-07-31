af3ebe4393be0804103094da0ec707e040b5cafe
Malik <malikhussein229@gmail.com>
2024-07-31 22:24:43 +0300

    Task#8 Ambient Namespaces

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -7,7 +7,6 @@ const row: RowElement = {
 };
 
 const newRowID: RowID = CRUD.insertRow(row);
-console.log(newRowID); // Output: 1
 const updateRow: RowElement = {...row, age: 23};
 CRUD.updateRow(newRowID, updateRow);
 CRUD.deleteRow(newRowID);
