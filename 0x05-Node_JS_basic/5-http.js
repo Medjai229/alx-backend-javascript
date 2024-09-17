8562845e39cffe850f3b7cd05626ee74cbea8f00
Malik <malikhussein229@gmail.com>
2024-09-17 21:49:36 +0300

    Task#5 Create a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,5 +1,33 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs').promises;
+
+async function countStudents(path) {
+  try {
+    const data = await fs.readFile(path, 'utf-8');
+
+    const lines = data.split('\n').filter((line) => line !== '');
+    lines.shift();
+    const students = lines.map((line) => line.split(','));
+
+    let result = `Number of students: ${students.length}\n`;
+
+    const fields = {};
+    students.forEach((student) => {
+      const field = student[3];
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(student[0]);
+    });
+
+    Object.keys(fields).forEach((field) => {
+      result += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
+    });
+    return result.trim();
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
 
 const app = http.createServer(async (req, res) => {
   if (req.url === '/') {
