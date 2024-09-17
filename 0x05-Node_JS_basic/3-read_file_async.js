759fc734feb5869cbe64028ef9f581a4900403a7
Malik <malikhussein229@gmail.com>
2024-09-17 20:44:15 +0300

    Task#3 Reading a file asynchronously with Node JS

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,30 @@
+const fs = require('fs/promises');
+
+async function countStudents(path) {
+  try {
+    const data = await fs.readFile(path, 'utf-8');
+
+    const lines = data.split('\n').filter((line) => line !== '');
+    lines.shift();
+    const students = lines.map((line) => line.split(','));
+
+    console.log(`Number of students: ${students.length}`);
+
+    const fields = {};
+    students.forEach((student) => {
+      const field = student[3];
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(student[0]);
+    });
+
+    Object.keys(fields).forEach((field) => {
+      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+    });
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
