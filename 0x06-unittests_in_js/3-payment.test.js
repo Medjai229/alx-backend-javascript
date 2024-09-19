2a882dd39f5511999740710802d63a0516497f1a
Malik <malikhussein229@gmail.com>
2024-09-19 22:47:46 +0300

    Task#3 Refactoring

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,3 +1,6 @@
+/* eslint-disable jest/prefer-expect-assertions */
+/* eslint-disable jest/valid-expect */
+/* eslint-disable no-unused-expressions */
 const { expect } = require('chai');
 const sinon = require('sinon');
 
@@ -5,23 +8,17 @@ const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
 
 describe('payment testing', () => {
-  let spy;
-
-  beforeEach(() => {
-    spy = sinon.spy(Utils, 'calculateNumber');
-  });
-
-  afterEach(() => {
-    spy.restore();
-  });
-
   it('should call Utils.calculateNumber with the correct arguments', () => {
     const totalAmount = 100;
     const totalShipping = 20;
+
+    const spy = sinon.spy(Utils, 'calculateNumber');
 
     sendPaymentRequestToApi(totalAmount, totalShipping);
 
     expect(spy.calledOnce).to.be.true;
     expect(spy.calledWith('SUM', totalAmount, totalShipping)).to.be.true;
+
+    spy.restore();
   });
 });
