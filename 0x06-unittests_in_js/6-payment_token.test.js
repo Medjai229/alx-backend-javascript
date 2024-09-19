4c5ed6be3251bb7ad312b47203ce03834484e716
Malik <malikhussein229@gmail.com>
2024-09-20 00:41:54 +0300

    Task#6 Adding done in it

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,12 +1,8 @@
-/* eslint-disable jest/prefer-expect-assertions */
-/* eslint-disable jest/valid-expect-in-promise */
-/* eslint-disable jest/valid-expect */
-/* eslint-disable no-undef */
 const { expect } = require('chai');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
-describe('getPaymentTokenFromAPI', (done) => {
-  it('should return a successful response when success is true', () => {
+describe('getPaymentTokenFromAPI', () => {
+  it('should return a successful response when success is true', (done) => {
     getPaymentTokenFromAPI(true)
       .then((response) => {
         expect(response).to.deep.equal({data: 'Successful response from the API'});
